63714572000af6da2dbcab9415f86d82208cc965
Lu Chao <deerchao@qq.com>
2018-04-16 18:45:46 +0800

    添加小程序基本支持代码

---
+++ src/Weixin.Next/WXA/Security.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace Weixin.Next.WXA
+{
+    public class Security
+    {
+        public static bool VerifySignature(string rawData, string sessionKey, string signature)
+        {
+            var input = Encoding.UTF8.GetBytes(rawData + sessionKey);
+            var output = SHA1.Create().ComputeHash(input);
+            return string.Concat(output.Select(x => x.ToString("x2"))).Equals(signature, StringComparison.OrdinalIgnoreCase);
+        }
+
+        public static string DecryptData(string encryptedData, string sessionKey, string iv)
+        {
+            var buffer = Convert.FromBase64String(encryptedData);
+
+            try
+            {
+                using (var rijndaelManaged = new RijndaelManaged
+                {
+                    Key = Convert.FromBase64String(sessionKey),
+                    IV = Convert.FromBase64String(iv),
+                    Mode = CipherMode.CBC
+                })
+                {
+                    using (var memoryStream = new MemoryStream(Convert.FromBase64String(encryptedData)))
+                    {
+                        using (var cryptoStream = new CryptoStream(memoryStream, rijndaelManaged.CreateDecryptor(), CryptoStreamMode.Read))
+                        {
+                            return new StreamReader(cryptoStream, Encoding.UTF8).ReadToEnd();
+                        }
+                    }
+                }
+            }
+            catch (CryptographicException)
+            {
+                return null;
+            }
+        }
+    }
+}
---
+++ src/Weixin.Next/WXA/Session.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Threading.Tasks;
+using Weixin.Next.Common;
+
+namespace Weixin.Next.WXA
+{
+    public class Session
+    {
+        /// <summary>
+        /// 以小程序 wx.login 获得的 code 换取用户唯一标识 openid 和 会话密钥 session_key
+        /// </summary>
+        /// <param name="appId">小程序唯一标识</param>
+        /// <param name="appSecret">小程序的 app secret</param>
+        /// <param name="jsCode">登录时获取的 code</param>
+        /// <param name="config"></param>
+        /// <returns></returns>
+        public static async Task<SessionResult> Get(string appId, string appSecret, string jsCode, ApiConfig config = null)
+        {
+            var url = $"https://api.weixin.qq.com/sns/jscode2session?grant_type=authorization_code&appid={Uri.EscapeDataString(appId)}&secret={Uri.EscapeDataString(appSecret)}&js_code={jsCode}";
+            var text = await ApiHelper.GetString(url, config).ConfigureAwait(false);
+            return ApiHelper.BuildResult<SessionResult>(text);
+        }
+
+        public class SessionResult : IApiResult
+        {
+            /// <summary>
+            /// 用户唯一标识
+            /// </summary>
+            public string openid { get; set; }
+
+            /// <summary>
+            /// 会话密钥
+            /// </summary>
+            public string session_key { get; set; }
+
+            /// <summary>
+            /// 用户在开放平台的唯一标识符
+            /// </summary>
+            public string unionid { get; set; }
+        }
+    }
+}
